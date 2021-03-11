722d7b8d67abc252445998a89821bb0f14403a3f
Vartika Chaudhary <36613708+VartikaChaudhary@users.noreply.github.com>
2021-03-11 14:04:29 +0530

    Create Check if a binary tree is subtree of another binary tree.py

---
+++ Check if a binary tree is subtree of another binary tree.py
@@ -0,0 +1,38 @@
+class Node: 
+	def __init__(self, data): 
+		self.data = data 
+		self.left = None
+		self.right = None
+
+  def areIdentical(node1, node2): 
+	 if node1 is None and node2 is None: 
+		return True
+	 if node1 is None or node2 is None: 
+		return False
+	 return (node1.data == node2.data and areIdentical(node1.left , node2.left) and areIdentical(node1.right, node2.right) ) 
+
+  def isSubtree(First, Second): 
+	 if Second is None: 
+		return True
+	 if First is None: 
+		return False
+	 if (areIdentical(First, Second)): 
+		return True
+ 	 return isSubtree(First.left, Second) or isSubtree(First.right, Second) 
+
+First = Node(26) 
+First.right = Node(3) 
+First.right.left = Node(3) 
+First.left = Node(10) 
+First.left.left = Node(4) 
+First.left.left.right = Node(30) 
+
+Second = Node(10) 
+Second.right = Node(6) 
+Second.left = Node(4) 
+Second.right.right = Node(50) 
+
+if isSubtree(First, Second): 
+	print "Tree 2 is subtree of Tree 1"
+else : 
+	print "Tree 2 is not a subtree of Tree 1"
